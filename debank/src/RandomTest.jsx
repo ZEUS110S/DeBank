19747f02463fd0ef95976efd799408d14aa0419f
dhdthdtr <nguyenvubang011001hiie@gmail.com>
2023-03-22 16:22:20 +0700

    fix API and create PDF

--- debank/src/RandomTest.jsx
+++ debank/src/RandomTest.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { useState } from "react";
 import axios from "axios";
+import {jsPDF} from 'jspdf';
+import { callAddFont } from "./calibri-normal";
 
 function RandomTest(){
     const [subject, setSubject] = useState("1");
     const [diff, setDiff] = useState("Dễ");
-    // const [json, setJson] = useState([])
 
     const subjectChange = (e) => {
         setSubject(e.target.value);
@@ -16,9 +17,10 @@ function RandomTest(){
     }
 
     const createForm = () => {
-        var subject_name = "Chemistry"
-        var difficulty = "easy"
+        var subject_name = document.querySelector("#subjects").value;
+        var difficulty = document.querySelector("#difficulties").value;
 
+        // get questions from db using API
         axios.get(
             "http://localhost:5133/api/Questions/SUBJECT_NAME,DIFFICULTY",
             {
@@ -28,23 +30,35 @@ function RandomTest(){
                 },
                 headers:{
                     'Access-Control-Allow-Origin': true
-                },
-                proxy:{
-                    protocol: 'https',
-                    host: "0799-115-76-54-96.ap.ngrok.io",
                 }
             }
         )
-        .then((res) => console.log(res.data))
-        .catch((err) => console.log(err))
+        .then(res => res.data)
+        .then(data => {
+            // init x and y for pdf text coordination
+            var x = 10;
+            var y = 10;
 
-        // fetch("https://0799-115-76-54-96.ap.ngrok.io/api/Questions",{
-        //     mode: "cors",
-        //     headers: {
-        //         'Access-Control-Allow-Origin': true
-        //     }
-        // })
-        // .then(res => res.json()).then(console.log);
+            var yA = 20;
+            var xA = 10;
+            // create PDF
+            jsPDF.API.events.push(["addFonts", callAddFont]);
+            const doc = new jsPDF();
+            
+            doc.setFont("calibri", "normal") // set custom font
+            doc.setFontSize(10) // set font size
+            data.forEach((question, index) => {
+                doc.text((index + 1) + ". " + question.questioN_TITLE, x, y, {maxWidth: 195})
+                doc.text("A. " + question.answeR_1, xA, yA, {maxWidth: 190})
+                doc.text("B. " + question.answeR_2, xA, yA + 5, {maxWidth: 190})
+                doc.text("C. " + question.answeR_3, xA, yA + 10, {maxWidth: 190})
+                doc.text("D. " + question.answeR_4, xA, yA + 15, {maxWidth: 190})
+                y += 35
+                yA += 35
+            }); // loop over all elements in array to add question to pdf
+            doc.save("a4.pdf") // save and download final pdf file
+        })
+        .catch((err) => console.log(err))        
     }
 
     return (
@@ -59,9 +73,9 @@ function RandomTest(){
                         <td id="td-question">Môn</td>
                         <td id="td-question">
                             <select name="subjects" id="subjects" value={subject} onChange={subjectChange}>
-                                <option value="1">Toán</option>
-                                <option value="2">Văn</option>
-                                <option value="3">Anh</option>
+                                <option value="Chemistry">Hoá</option>
+                                <option value="Biology">Sinh</option>
+                                <option value="English">Anh</option>
                             </select>
                         </td>
                     </tr>
@@ -69,28 +83,18 @@ function RandomTest(){
                         <td id="td-question">Độ khó</td>
                         <td id="td-question">
                         <select name="difficulties" id="difficulties" value={diff} onChange={diffChange}>
-                            <option value="Dễ">Dễ</option>
-                            <option value="Thường">Thường</option>
-                            <option value="Khó">Khó</option>
+                            <option value="easy">Dễ</option>
+                            <option value="normal">Thường</option>
+                            <option value="hard">Khó</option>
                         </select>
                         </td>
                     </tr>
                 </tbody>
             </table>
             <div style={{margin: '10px auto 0 auto', width: '50px'}}>
-                <button id="downloadPDF" onClick={createForm}>submit</button>
+                <button id="downloadPDF" onClick={() => createForm()}>submit</button>
             </div>
         </div>
     )
 }
-// () => createForm()
-
-// await axios.get(
-        //     "https://284f-2402-800-639a-c0b8-2460-1d2d-62c1-cbfb.ap.ngrok.io/api/questions?SUBJECT_ID="+subject, 
-        //     {
-        //         headers:{
-        //             'Access-Control-Allow-Origin': '*'
-        //         }
-        //     }
-        // )
 export default RandomTest
