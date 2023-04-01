25635cc16e14e8dc7ef257d1f1b8bb77a9364b41
dhdthdtr <nguyenvubang011001hiie@gmail.com>
2023-04-01 16:51:59 +0700

    add update question

--- debank/src/RandomQuestion.jsx
+++ debank/src/RandomQuestion.jsx
@@ -21,7 +21,7 @@ function RandomQuestion(props){
     const [updateSubject, setUpdateSubject] = useState(question.subjecT_NAME === null ? "Math" : question.subjecT_NAME);
     const [updateDiff, setUpdateDiff] = useState(question.difficulty);
     const [updateGrade, setUpdateGrade] = useState(question.grade);
-    const [updateIsCorrect, setUpdateIsCorrect] = useState("A");
+    const [updateIsCorrect, setUpdateIsCorrect] = useState(question.answer);
     const [updateAnswer1, setUpdateAnswer1] = useState(question.answeR_1)
     const [updateAnswer2, setUpdateAnswer2] = useState(question.answeR_2)
     const [updateAnswer3, setUpdateAnswer3] = useState(question.answeR_3)
@@ -110,13 +110,76 @@ function RandomQuestion(props){
         var isCorrectAnswer = document.querySelector("#isCorrect").value;
         var grade = document.querySelector('#grade').value;
 
-        console.log(subjects + " " + diffs)
-
-        props.addQuestion(questionTitle, subjects, props.userID, answer1, answer2, answer3, answer4, isCorrectAnswer, diffs, grade)
+        if(questionTitle === "" ||
+           answer1 === "" ||
+           answer2 === "" ||
+           answer3 === "" ||
+           answer4 === "" ||
+           !questionTitle.replace(/\s/g, '').length ||
+           !answer1.replace(/\s/g, '').length ||
+           !answer2.replace(/\s/g, '').length ||
+           !answer3.replace(/\s/g, '').length ||
+           !answer4.replace(/\s/g, '').length){
+            alert("Hãy nhập đầy đủ các trường")
+        } else {
+            console.log(subjects + " " + diffs)
+            props.addQuestion(questionTitle, subjects, props.userID, answer1, answer2, answer3, answer4, isCorrectAnswer, diffs, grade)
+        }
     }
 
     const update = () => {
         console.log(question)
+
+        var questionTitle = document.querySelector("#questionTitle").value;
+        var subjects = document.querySelector("#subjects").value;
+        var diffs = document.querySelector("#difficulties").value;
+        var answer1 = document.querySelector("#answer1").value;
+        var answer2 = document.querySelector("#answer2").value;
+        var answer3 = document.querySelector("#answer3").value;
+        var answer4 = document.querySelector("#answer4").value;
+        var isCorrectAnswer = document.querySelector("#isCorrect").value;
+        var grade = document.querySelector('#grade').value;
+        var subjectID;
+
+        if(questionTitle === "" ||
+           answer1 === "" ||
+           answer2 === "" ||
+           answer3 === "" ||
+           answer4 === "" ||
+           !questionTitle.replace(/\s/g, '').length ||
+           !answer1.replace(/\s/g, '').length ||
+           !answer2.replace(/\s/g, '').length ||
+           !answer3.replace(/\s/g, '').length ||
+           !answer4.replace(/\s/g, '').length){
+            alert("Hãy nhập đầy đủ các trường")
+        } else {
+            switch(subjects){
+                case "Math":
+                    subjectID = 1;
+                    break;
+                case "Chemistry":
+                    subjectID = 2;
+                    break;
+                case "Physics":
+                    subjectID = 3;
+                    break;
+                case "Biology":
+                    subjectID = 4;
+                    break;
+                case "History":
+                    subjectID = 5;
+                    break;
+                case "Geography":
+                    subjectID = 6;
+                    break;
+                case "English":
+                    subjectID = 7;
+                    break;
+                default:
+                    break;
+            }
+            props.updateQuestion(question.questioN_ID, questionTitle, subjectID, props.userID, answer1, answer2, answer3, answer4, isCorrectAnswer, diffs, grade);
+        }
     }
 
     if(editFlg === '1'){
@@ -169,25 +232,25 @@ function RandomQuestion(props){
                         <tr id="tr-question">
                             <td id="td-question">A</td>
                             <td id="td-question">
-                                <input type="text" id="answer1" name="answer1" style={{width: '300px'}} value={answer1} onChange={answer1Change}/>
+                                <input type="text" id="answer1" name="answer1" style={{width: '300px'}} value={answer1} onChange={answer1Change} placeholder="Đáp án A"/>
                             </td>
                         </tr>
                         <tr id="tr-question">
                             <td id="td-question">B</td>
                             <td id="td-question">
-                                <input type="text" id="answer2" name="answer2" style={{width: '300px'}} value={answer2} onChange={answer2Change}/>
+                                <input type="text" id="answer2" name="answer2" style={{width: '300px'}} value={answer2} onChange={answer2Change} placeholder="Đáp án B"/>
                             </td>
                         </tr>
                         <tr id="tr-question">
                             <td id="td-question">C</td>
                             <td id="td-question">
-                                <input type="text" id="answer3" name="answer3" style={{width: '300px'}} value={answer3} onChange={answer3Change}/>
+                                <input type="text" id="answer3" name="answer3" style={{width: '300px'}} value={answer3} onChange={answer3Change} placeholder="Đáp án C"/>
                             </td>
                         </tr>
                         <tr id="tr-question">
                             <td id="td-question">D</td>
                             <td id="td-question">
-                                <input type="text" id="answer4" name="answer4" style={{width: '300px'}} value={answer4} onChange={answer4Change}/>
+                                <input type="text" id="answer4" name="answer4" style={{width: '300px'}} value={answer4} onChange={answer4Change} placeholder="Đáp án D"/>
                             </td>
                         </tr>
                         <tr id="tr-question">
@@ -260,31 +323,31 @@ function RandomQuestion(props){
                         <tr id="tr-question">
                             <td id="td-question">A</td>
                             <td id="td-question">
-                                <input type="text" id="answer1" name="answer1" style={{width: '300px'}} value={updateAnswer1} onChange={updateAnswer1Change}/>
+                                <input type="text" id="answer1" name="answer1" spellCheck="false" style={{width: '300px'}} value={updateAnswer1} onChange={updateAnswer1Change} placeholder="Đáp án A"/>
                             </td>
                         </tr>
                         <tr id="tr-question">
                             <td id="td-question">B</td>
                             <td id="td-question">
-                                <input type="text" id="answer2" name="answer2" style={{width: '300px'}} value={updateAnswer2} onChange={updateAnswer2Change}/>
+                                <input type="text" id="answer2" name="answer2" spellCheck="false" style={{width: '300px'}} value={updateAnswer2} onChange={updateAnswer2Change} placeholder="Đáp án B"/>
                             </td>
                         </tr>
                         <tr id="tr-question">
                             <td id="td-question">C</td>
                             <td id="td-question">
-                                <input type="text" id="answer3" name="answer3" style={{width: '300px'}} value={updateAnswer3} onChange={updateAnswer3Change}/>
+                                <input type="text" id="answer3" name="answer3" spellCheck="false" style={{width: '300px'}} value={updateAnswer3} onChange={updateAnswer3Change} placeholder="Đáp án C"/>
                             </td>
                         </tr>
                         <tr id="tr-question">
                             <td id="td-question">D</td>
                             <td id="td-question">
-                                <input type="text" id="answer4" name="answer4" style={{width: '300px'}} value={updateAnswer4} onChange={updateAnswer4Change}/>
+                                <input type="text" id="answer4" name="answer4" spellCheck="false" style={{width: '300px'}} value={updateAnswer4} onChange={updateAnswer4Change} placeholder="Đáp án D"/>
                             </td>
                         </tr>
                         <tr id="tr-question">
                             <td id="td-question">Đáp án</td>
                             <td id="td-question">
-                                <select name="isCorrect" id="isCorrect" value={isCorrect} onChange={updateIsCorrectChange}>
+                                <select name="isCorrect" id="isCorrect" value={updateIsCorrect} onChange={updateIsCorrectChange}>
                                     <option value="A">A</option>
                                     <option value="B">B</option>
                                     <option value="C">C</option>
@@ -296,6 +359,9 @@ function RandomQuestion(props){
                 </table>
                 <div id="update-btn">
                     <button onClick={() => update()}>Cập nhật câu hỏi</button>
+                </div>
+                <div id="success-text">
+                    <span>Cập nhật thành công</span>
                 </div>
             </div>
         )
